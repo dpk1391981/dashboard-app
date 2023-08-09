7241bdc016f3424118b9ff3cc175041ff652d935
Deepak Kumar <deepak.kumar@instantsys.com>
2023-08-09 18:22:07 +0530

    add config file

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+let mongoURI =
+  process.env.NODE_ENV == "production" ? db["production"] : db["local"];
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log(`MongoDB Connected...`);
+  } catch (error) {
+    console.log(`Error @connectDB: ${error.message}`);
+
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
